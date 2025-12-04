3df4218e9553fcf4521fd1a9574b75f5f31ef82c
Adrian Ruiz <addsdeveloper@gmail.com>
2025-12-04 12:17:30 -0500

    Refactor Main class to implement advanced filtering of campers by age and gender

--- src/main/java/com/adrian/Main.java
+++ src/main/java/com/adrian/Main.java
@@ -7,78 +7,78 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 import com.adrian.domain.Camper;
+import com.adrian.domain.Sexo;
 
 public class Main {
     public static void main(String[] args) {
 
-        Map<String, Integer> mercado = Map.of(
-                "Pollo", 3,
-                "Arroz", 7,
-                "Frutas", 2,
-                "Banano", 20);
-
         List<Camper> campers = Arrays.asList(
                 new Camper("Juan", 21, LocalDate.of(1998, 11, 6)),
-                new Camper("Sara", 17, LocalDate.of(2000, 10, 30)),
-                new Camper("Laura", 27, LocalDate.of(1995, 5, 12)),
-                new Camper("Cristian", 20, LocalDate.of(2005, 2, 2)));
+                new Camper("Sara", 17, LocalDate.of(2000, 10, 30), Sexo.FEMENINO),
+                new Camper("Laura", 27, LocalDate.of(1995, 5, 12), Sexo.FEMENINO),
+                new Camper("Cristian", 20, LocalDate.of(2010, 2, 2)),
+                new Camper("Chuki", 20, LocalDate.of(2010, 2, 2), Sexo.OTRO));
 
-        Comparator<Camper> comparatorAsc = (p1, p2) -> Integer.compare(p1.getEdad(), p2.getEdad());
-        Comparator<Camper> comparatorDesc = (p1, p2) -> Integer.compare(p2.getEdad(), p1.getEdad());
-        Comparator<Camper> comparatorDate = (p1, p2) -> p2.getFecha().compareTo(p1.getFecha());
-
-        Consumer<Camper> print = (c) -> {
-            System.out.println("Informacion Camper");
-            System.out.println("Nombre:\t" + c.getNombre());
-            System.out.println("Edad:\t" + c.getEdad());
-        };
-
-        Consumer<Camper> printWithDate = (c) -> {
-            System.out.println("Informacion Camper");
-            System.out.println("Nombre:\t" + c.getNombre());
-            System.out.println("Edad:\t" + c.getEdad());
-            System.out.println("Fecha:\t" + c.getFecha().toString());
-        };
-
-
-        mostrarDatos(campers, comparatorDate, printWithDate);
-        System.out.println("+++++++++++++++++++++++++++++");
-        mostrarDatos(campers, comparatorAsc, print);
-        System.out.println("+++++++++++++++++++++++++++++");
-        mostrarDatos(campers, comparatorDesc, printWithDate);
-
-        System.out.println("=============================");
-        BiConsumer<String, Integer> printMap = (k,v) -> {
-            System.out.println("Mercado Libre: ");
-            System.out.println(k+ ":\t" + v + " u");
-        };
-        mostrarDatos(mercado, printMap);
-
-        Supplier<Integer> edad = () -> {
-            campers.sort((c1,c2) -> c1.getEdad() - c2.getEdad());
-            return campers.get(0).getEdad();
-        };
-
-        System.out.println("La edad mayor del grupo de campers es: " + edadMayor(campers));
-        System.out.println("La edad mayor del grupo de campers es: " + edad.get());
-
-
+        filtradoAvanzado(campers);
     }
 
     public static int edadMayor(List<Camper> datos) {
-        datos.sort((c1,c2) -> c1.getEdad() - c2.getEdad());
+        datos.sort((c1, c2) -> c1.getEdad() - c2.getEdad());
         return datos.get(0).getEdad();
-    } 
+    }
 
-    public static void mostrarDatos(Map<String,Integer> datos, BiConsumer<String,Integer> print) {
+    public static void mostrarDatos(Map<String, Integer> datos, BiConsumer<String, Integer> print) {
         datos.forEach(print);
     }
 
     public static void mostrarDatos(List<Camper> datos, Comparator<Camper> orden, Consumer<Camper> print) {
         datos.sort(orden);
         datos.forEach(print);
+    }
+
+    public static void filtradoAvanzado(List<Camper> campers) {
+        var streamCamers = campers.stream()
+                .filter((c) -> {
+                    var result = c.getFecha().compareTo(LocalDate.now().minusYears(18)) < 0;
+                    // System.out.println(c.getNombre() + " : " + result);
+                    return result;
+                })
+                .filter((c) -> c.getSexo() == Sexo.FEMENINO)
+                // .peek(printWithDate)
+                .map((c) -> c.getNombre() + " -> " + c.getEdad())
+                // .peek((nuevo) -> System.out.println(nuevo))
+                .toList();
+
+        System.out.println("Fin del filtrado: " + streamCamers.size());
+        System.out.println("Lista original: " + campers.size());
+
+        var total = campers.size();
+        var masculinos = campers.stream()
+                .filter((camper) -> camper.getSexo() == Sexo.MASCULINO)
+                .map((c) -> 1)
+                .reduce(0, (acomulador, item) -> acomulador + item);
+
+        var femeninos = campers.stream()
+                .filter((camper) -> camper.getSexo() == Sexo.FEMENINO)
+                .map((c) -> 1)
+                .reduce(0, (acomulador, item) -> acomulador + item);
+        var otros = campers.stream()
+                .filter((camper) -> camper.getSexo() == Sexo.OTRO)
+                .map((c) -> 1)
+                .reduce(0, (acomulador, item) -> acomulador + item);
+
+        System.out.println("Total: " + total);
+        System.out.println("Masculinos: " + masculinos);
+        System.out.println("Femeninos: " + femeninos);
+        System.out.println("Otros: " + otros);
+
+        // Filtrar campers mayores de edad
+        // Obtener los nombres de las campers
+        // Calcular la suma de las edades
+        // Calcular el promedio de las edades
+        // Calcular el promedio de las edades por Sexo
+
     }
 }
